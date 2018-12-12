662530618aecc21a338ca79e30e6a57807d7c9ce
MKR <25127328+maykar@users.noreply.github.com>
2018-12-11 21:09:27 -0500

    Refactor

--- compact-custom-header/compact-custom-header.js
+++ compact-custom-header/compact-custom-header.js
@@ -1,50 +1,33 @@
 class CompactCustomHeader extends HTMLElement {
   set hass(hass) {
-    let header = String(
-      this.config.header).replace(/\s+/g, '').split(',');
-    let menu = String(
-      this.config.menu).replace(/\s+/g, '').split(',');
-    let notify = String(
-      this.config.notification).replace(/\s+/g, '').split(',');
-    let voice = String(
-      this.config.voice).replace(/\s+/g, '').split(',');
-    let options = String(
-      this.config.options).replace(/\s+/g, '').split(',');
-    let clock = String(
-      this.config.clock).replace(/\s+/g, '').split(',');
-    let clock_format = String(
-      this.config.clock_format).replace(/\s+/g, '').split(',');
-    let clock_am_pm = String(
-      this.config.clock_am_pm).replace(/\s+/g, '').split(',');
-
+    let header = format_config(this.config.header);
+    let menu = format_config(this.config.menu);
+    let notify = format_config(this.config.notification);
+    let voice = format_config(this.config.voice);
+    let options = format_config(this.config.options);
+    let clock = format_config(this.config.clock);
+    let clock_format = format_config(this.config.clock_format);
+    let clock_am_pm = format_config(this.config.clock_am_pm);
     let user_agent = ',' + this.config.user_agent;
     user_agent = user_agent.split(',');
-    let agent = 0;
+    let ua = 0;
     for (let i = 0; i < user_agent.length; i++) {
       let regex = new RegExp(user_agent[i], 'i');
       if (regex.test(navigator.userAgent)) {
-        agent = i;
+        ua = i;
       }
     }
-    window.cch_header = config_default(
-      header[0], header[agent], true, true);
-    window.cch_menu = config_default(
-      menu[0], menu[agent], true, true);
-    window.cch_notify = config_default(
-      notify[0], notify[agent], true, true);
-    window.cch_voice = config_default(
-      voice[0], voice[agent], true, true);
-    window.cch_options = config_default(
-      options[0], options[agent], true, true);
-    window.cch_clock = config_default(
-      clock[0], clock[agent], false, false);
-    window.cch_clock_format = config_default(
-      clock_format[0], clock_format[agent], 12, false);
-    window.cch_am_pm = config_default(
-      clock_am_pm[0], clock_am_pm[agent], true, true);
+    window.cch_header = conf_def(header[0], header[ua], true);
+    window.cch_menu = conf_def(menu[0], menu[ua], true);
+    window.cch_notify = conf_def(notify[0], notify[ua], true);
+    window.cch_voice = conf_def(voice[0], voice[ua], true);
+    window.cch_options = conf_def(options[0], options[ua], true);
+    window.cch_clock = conf_def(clock[0], clock[ua], false);
+    window.cch_clock_format = conf_def(clock_format[0], clock_format[ua], 12);
+    window.cch_am_pm = conf_def(clock_am_pm[0], clock_am_pm[ua], true);
     const script = document.createElement('script');
     script.src = '/local/custom-lovelace/compact-custom-header/' +
-                 'compact-custom-header.lib.js?v0.0.3009';
+                 'compact-custom-header.lib.js?v0.0.4';
     document.head.appendChild(script);
     window.dispatchEvent(new Event('resize'));
   }
@@ -55,19 +38,24 @@ class CompactCustomHeader extends HTMLElement {
     return 0;
   }
 }
-
-function config_default(default_agent, this_agent, default_value, is_bool) {
-  if (this_agent == undefined) {
-    if (default_agent == undefined) {
-      return default_value;
+// Config to string, to list, and strip spaces.
+function format_config(config) {
+  return String(config).replace(/\s+/g, '').split(',');
+}
+// Configuration and defaults. User-agent > main-config > default.
+function conf_def(main_ua, this_ua, default_val) {
+  if (this_ua == undefined) {
+    if (main_ua == undefined) {
+      return default_val;
     } else {
-      let x = is_bool ? (default_agent == 'true') : default_agent;
+      let x = main_ua == 'true' || main_ua == 'false' ?
+        (main_ua == 'true') : main_ua;
       return x;
     }
   } else {
-    let x = is_bool ? (this_agent == 'true') : this_agent;
+    let x = this_ua == 'true' || this_ua == 'false' ?
+      (this_ua == 'true') : this_ua;
     return x;
   }
 }
-
 customElements.define('compact-custom-header', CompactCustomHeader);
