6738a734686a5a0c3f4b15f876f8a0a6466a12dc
MKR <25127328+maykar@users.noreply.github.com>
2018-12-29 17:34:33 -0500

    Remove clock function to help with config changes & cleanup

--- compact-custom-header/compact-custom-header.lib.js
+++ compact-custom-header/compact-custom-header.lib.js
@@ -1,17 +1,15 @@
 // Avoid "already defined" errors when navigating away from Lovelace and back.
 if (doc_root == undefined) {
-  var app_layout, card, clock, clock_format,clock_width, div_view, doc_root,
-      drawer_layout, edit_mode, hui_root, icon, iron_icon, love_lace, main,
-      menu_btn, menu_icon, menu_iron_icon, notify_btn, notify_icon,
-      notify_indicator, notify_iron_icon, options_button, options_icon,
-      options_iron_icon, pad, pages, panel, proceed, shift_amount, tabs,
-      tabs_container, chevron, tabs_count, toolbar, voice_btn, voice_icon,
-      voice_iron_icon, raw_config;
+  var app_layout, card, chevron, div_view, doc_root, drawer_layout, edit_mode,
+      hui_root, icon, iron_icon, love_lace, main, menu_btn, menu_icon,
+      menu_iron_icon, notify_btn, notify_icon, notify_dot, notify_iron_icon,
+      options_btn, options_icon, options_iron_icon, pages, panel, proceed,
+      raw_config, tabs, tabs_container, tabs_count, toolbar, voice_btn,
+      voice_icon, voice_iron_icon;
 }
-// Try so that if we're not on a lovelace page it won't continue to run...
-// since some elements only exist on lovelace & we insert script in doc head.
+// Try so that if we're not on a lovelace page it won't continue to run.
 try {
-  // Get the parents of the child elements to style.
+  // Get the parents of the elements to style.
   doc_root = document.querySelector('home-assistant').shadowRoot;
   main = doc_root.querySelector('home-assistant-main').shadowRoot;
   drawer_layout = main.querySelector('app-drawer-layout');
@@ -49,7 +47,7 @@ if (proceed) {
     edit_mode = false;
   }
 
-  // CSS for card.
+  // Card styling.
   let button_style = `
     margin:auto;
     margin-bottom:10px;
@@ -98,7 +96,7 @@ if (proceed) {
     } else {
       card.parentNode.style.cssText = '';
     }
-    // Create and display card in edit mode.
+    // Create and display card in edit & raw config modes.
     if (edit_mode || raw_config) {
       let ua_text = window.cch_ua_display ? 'Hide' : 'Show';
       let tabs_text = window.cch_tabs_display ? 'Revert' : 'Show';
@@ -148,13 +146,14 @@ if (proceed) {
     notify_btn = hui_root.querySelector('hui-notifications-button');
     notify_icon = notify_btn.shadowRoot.querySelector('paper-icon-button');
     notify_iron_icon = notify_icon.shadowRoot.querySelector('iron-icon');
-    notify_indicator = notify_btn.shadowRoot.querySelector('[class="indicator"]');
+    notify_dot = notify_btn.shadowRoot.querySelector('[class="indicator"]');
     voice_btn = hui_root.querySelector('ha-start-voice-button');
     voice_icon = voice_btn.shadowRoot.querySelector('paper-icon-button');
     voice_iron_icon = voice_icon.shadowRoot.querySelector('iron-icon');
-    options_button = hui_root.querySelector('paper-menu-button');
-    options_icon = options_button.querySelector('paper-icon-button');
+    options_btn = hui_root.querySelector('paper-menu-button');
+    options_icon = options_btn.querySelector('paper-icon-button');
     options_iron_icon = options_icon.shadowRoot.querySelector('iron-icon');
+    
     // Hide scroll arrows on tab bar to save space.
     chevron[0].style.cssText = 'display:none;';
     chevron[1].style.cssText = 'display:none;';
@@ -176,7 +175,7 @@ if (proceed) {
     }
 
     // Add width of all visable elements on right side for tabs margin.
-    pad = 20;
+    let pad = 20;
     pad += window.cch_notify && window.cch_clock != 'notification' ? 40 : 0;
     pad += window.cch_voice && window.cch_clock != 'voice' ? 40 : 0;
     pad += window.cch_options && window.cch_clock != 'options' ? 56 : 0;
@@ -186,6 +185,9 @@ if (proceed) {
     }
     tabs.style.cssText = `margin-right:${pad}px;`;
 
+    // Set width of clock based on format options.
+    let clock_width = window.cch_clock_format == 12 && window.cch_am_pm ?
+      90 : 70;
     // Add margin to the left side if the menu button is a clock.
     if (window.cch_menu && window.cch_clock != 'menu') {
       tabs_container.style.cssText = 'margin-left:60px;';
@@ -193,12 +195,15 @@ if (proceed) {
       tabs_container.style.cssText = `margin-left:${clock_width + 15}px;`;
     }
 
+    // Strings to compare config to. Avoids errors while typing in edit field.
+    let clock_strings = ['notification','voice','options','menu'];
+
     // Get elements to style for clock choice.
-    if (window.cch_clock) {
+    if (clock_strings.indexOf(window.cch_clock) > -1) {
       if (window.cch_clock == 'notification') {
         icon = notify_icon;
         iron_icon = notify_iron_icon;
-        notify_indicator.style.cssText = 'top:14.5px;left:-7px';
+        notify_dot.style.cssText = 'top:14.5px;left:-7px';
       } else if (window.cch_clock == 'voice') {
         icon = voice_icon;
         iron_icon = voice_iron_icon;
@@ -209,22 +214,7 @@ if (proceed) {
         icon = menu_icon;
         iron_icon = menu_iron_icon;
       }
-
-      // Find inserted clock element.
-      try {
-        clock = icon.shadowRoot.getElementById('cch_clock');
-      } catch (e) {
-        console.log(e);
-      }
-
-      clock_width = window.cch_clock_format == 12 && window.cch_am_pm ? 90 : 70;
-      clock_format = {
-        'hour12': (window.cch_clock_format != 24),
-        'hour': '2-digit',
-        'minute': '2-digit'
-      };
-
-      // If the clock element doesn't exist yet, create it.
+      // If the clock element doesn't exist yet, create & insert.
       if (window.cch_clock && clock == null) {
         let create_clock = document.createElement('p');
         create_clock.setAttribute('id','cch_clock');
@@ -234,40 +224,44 @@ if (proceed) {
           margin-left:-8px;
         `;
         iron_icon.parentNode.insertBefore(create_clock, iron_icon);
-      // If the clock element exists, style it's container and hide the icon.
-      } else if (window.cch_clock && clock != null) {
-        icon_clock();
+      }
+      // Style clock and insert time text.
+      var clock = icon.shadowRoot.getElementById('cch_clock');
+      if (window.cch_clock && clock != null) {
+        let clock_format = {
+          'hour12': (window.cch_clock_format != 24),
+          'hour': '2-digit',
+          'minute': '2-digit'
+        };
+        let date = new Date();
+        date = date.toLocaleTimeString([], clock_format);
+        if (!window.cch_am_pm && window.ch_clock_format == 12) {
+          clock.innerHTML = date.slice(0, -3);
+        } else {
+          clock.innerHTML = date;
+        }
         icon.style.cssText = `
           margin-right:-5px;
           width:${clock_width}px;
           text-align: center;
         `;
         iron_icon.style.cssText = 'display:none;';
+        window.dispatchEvent(new Event('resize'));
       }
     }
-  } else if (window.cch_disable) {
-    // Clear any styles that were modified if disabled.
-    chevron[0].style.cssText = '';
-    chevron[1].style.cssText = '';
-    hui_root.querySelector('app-header').style.cssText = '';
-    hui_root.querySelector('app-toolbar').style.cssText = '';
-    tabs.style.cssText = '';
-    tabs_container.style.cssText = '';
-    notify_indicator.style.cssText = '';
-    if (clock != null) clock.style.cssText = '';
-    if (iron_icon != null) iron_icon.style.cssText = '';
-    if (icon != null) icon.style.cssText = '';
-    if (clock != null) clock.innerHTML = '';
-    for (let i = 0; i < tabs_count.length; i++) {
-      tabs_count[i].style.cssText = '';
-    }
   }
+
+  // Remove clock from element if no longer set.
+  remove_clock('notification', notify_icon, notify_btn);
+  remove_clock('voice', voice_icon, voice_btn);
+  remove_clock('options', options_icon, options_btn);
+  remove_clock('menu', menu_icon, menu_btn);
 
   // Hide or show buttons and tab container
   element_style(window.cch_menu, menu_btn, true);
   element_style(window.cch_notify, notify_btn, true);
   element_style(window.cch_voice, voice_btn, true);
-  element_style(window.cch_options, options_button, true);
+  element_style(window.cch_options, options_btn, true);
   if (window.cch_tabs_display == undefined) {
     for (let i = 0; i < tabs_count.length; i++) {
       element_style(window.cch_tabs, tabs_count[i], false);
@@ -282,14 +276,14 @@ function recursive_walk(node, func) {
     var done = func(node) || node.nodeName == 'COMPACT-CUSTOM-HEADER';
     if (done) return true;
     if ('shadowRoot' in node && node.shadowRoot) {
-        done = recursive_walk(node.shadowRoot, func);
-        if (done) return true;
+      done = recursive_walk(node.shadowRoot, func);
+      if (done) return true;
     }
     node = node.firstChild;
     while (node) {
-        done = recursive_walk(node, func);
-        if (done) return true;
-        node = node.nextSibling;
+      done = recursive_walk(node, func);
+      if (done) return true;
+      node = node.nextSibling;
     }
 }
 
@@ -325,10 +319,12 @@ function show_all_tabs() {
 
 // Style or hide buttons and tabs.
 function element_style(config, element, shift) {
-  shift_amount = edit_mode ? 240 : 111;
+  let top = edit_mode ? 240 : 111;
+  let options_style = element == options_btn ?
+    'margin-right:-5px; padding:0;' : '';
   if (tabs_count.length > 1 && shift && !window.cch_disable) {
     element.style.cssText = config ?
-      `z-index:1; margin-top:${shift_amount}px;` :
+      `z-index:1; margin-top:${top}px;${options_style}` :
       'display:none' ;
   } else if (!window.cch_disable){
     element.style.cssText = config ?
@@ -339,13 +335,19 @@ function element_style(config, element, shift) {
   }
 }
 
-// Create and insert text for the clock.
-function icon_clock() {
-  let date = new Date();
-  if (!window.cch_am_pm && window.ch_clock_format == 12) {
-    clock.innerHTML = date.toLocaleTimeString([], clock_format).slice(0, -3);
-  } else {
-    clock.innerHTML = date.toLocaleTimeString([], clock_format);
+function remove_clock(config, element, parent) {
+  if (window.cch_clock != config &&
+      element.shadowRoot.getElementById('cch_clock') != null) {
+    let clock_element = element.shadowRoot.getElementById('cch_clock');
+    clock_element.parentNode.querySelector('iron-icon').style.cssText = '';
+    if (config == 'options') {
+      parent.querySelector('paper-icon-button').style.cssText = '';
+    } else {
+      parent.shadowRoot.querySelector('paper-icon-button').style.cssText = '';
+    }
+    if (config == 'notification') {
+      notify_dot.style.cssText = '';
+    }
+    clock_element.parentNode.removeChild(clock_element);
   }
-  window.dispatchEvent(new Event('resize'));
 }
