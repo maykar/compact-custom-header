2cda98699f93f1cf67da085eecec41105302fa8f
MKR <25127328+maykar@users.noreply.github.com>
2018-12-30 02:38:12 -0500

    Add per user agent tab views

--- compact-custom-header/compact-custom-header.js
+++ compact-custom-header/compact-custom-header.js
@@ -7,6 +7,7 @@ class CompactCustomHeader extends HTMLElement {
       card.appendChild(this.content);
       this.appendChild(card);
     }
+    let tab_visability = this.config.tab_visability;
     let header = format_config(this.config.header);
     let menu = format_config(this.config.menu);
     let notify = format_config(this.config.notification);
@@ -32,6 +33,7 @@ class CompactCustomHeader extends HTMLElement {
       }
     }
     // Global variables for the main script.
+    window.cch_show_tabs = tab_visability[uai].replace(/\s+/g, '').split(',');
     window.cch_header = conf_def(header[0], header[uai], true);
     window.cch_menu = conf_def(menu[0], menu[uai], true);
     window.cch_notify = conf_def(notify[0], notify[uai], true);
