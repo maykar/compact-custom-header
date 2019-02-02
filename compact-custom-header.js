411e444b819247281fa8a8a86ca2f916fd14515a
MKR <contact@ninjacyb.org>
2019-02-02 15:12:33 -0500

    insertClock() & update a few names...
    notification to notifications
    cardConfig to cchConfig
    insertScript() to buildConfig()

--- compact-custom-header.js
+++ compact-custom-header.js
@@ -4,7 +4,7 @@ var html = LitElement.prototype.html;
 const defaultConfig = {
   header: true,
   menu: true,
-  notification: true,
+  notifications: true,
   voice: true,
   options: true,
   tabs: true,
@@ -36,7 +36,7 @@ class CompactCustomHeader extends LitElement {
 
   updated() {
     if (this.config && this.hass && this.firstRun) {
-      this.insertScript();
+      this.buildConfig();
     }
   }
 
@@ -51,9 +51,9 @@ class CompactCustomHeader extends LitElement {
           <svg viewBox="0 0 24 24">
             <path
               d="M12,7L17,12H14V16H10V12H7L12,7M19,
-        21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,
-        3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,
-        21M19,19V5H5V19H19Z"
+              21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,
+              3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,
+              21M19,19V5H5V19H19Z"
             ></path>
           </svg>
           <h2>Compact Custom Header</h2>
@@ -70,8 +70,8 @@ class CompactCustomHeader extends LitElement {
           </div>
           <div ?hidden=${!this.show_ua}>
             <textarea class="user_agent" rows="4" readonly>
-${navigator.userAgent}</textarea
-            >
+              ${navigator.userAgent}
+            </textarea>
           </div>
         </div>
       </ha-card>
@@ -106,7 +106,7 @@ ${navigator.userAgent}</textarea
     `;
   }
 
-  insertScript() {
+  buildConfig() {
     if (this.firstRun) {
       this.firstRun = false;
       this.userVars = {
@@ -116,7 +116,6 @@ ${navigator.userAgent}</textarea
     }
     let exceptionConfig = {};
     let highestMatch = 0;
-
     if (this.config.exceptions) {
       this.config.exceptions.forEach(exception => {
         const matches = this.countMatches(exception.conditions);
@@ -126,8 +125,7 @@ ${navigator.userAgent}</textarea
         }
       });
     }
-
-    this.cardConfig = { ...defaultConfig, ...this.config, ...exceptionConfig };
+    this.cchConfig = { ...defaultConfig, ...this.config, ...exceptionConfig };
     this.run();
   }
 
@@ -168,117 +166,75 @@ ${navigator.userAgent}</textarea
         root.querySelectorAll("app-toolbar")[0].className == "edit-mode";
 
       // Style header and icons.
-      if (!this.cardConfig.disable && !this.raw_config_mode) {
+      if (!this.cchConfig.disable && !this.raw_config_mode) {
         this.button = {};
         this.button.menu = root.querySelector("ha-menu-button");
-        this.button.notification = root.querySelector(
-          "hui-notifications-button"
-        );
         this.button.voice = root.querySelector("ha-start-voice-button");
         this.button.options = root.querySelector("paper-menu-button");
+        this.button.notifications = root.querySelector(
+          "hui-notifications-button"
+        );
 
         // Hide header completely if set to false in config.
-        if (!this.cardConfig.header) {
+        if (!this.cchConfig.header) {
           root.querySelector("app-header").style.cssText = "display:none;";
         }
 
-        // Remove clock from element if no longer set.
-        this.removeClock(
-          "notification",
-          this.button.notification.shadowRoot.querySelector(
-            "paper-icon-button"
-          ),
-          this.button.notification
-        );
-        this.removeClock(
-          "voice",
-          this.button.voice.shadowRoot.querySelector("paper-icon-button"),
-          this.button.voice
-        );
-        this.removeClock(
-          "options",
-          this.button.options.querySelector("paper-icon-button"),
-          this.button.options
-        );
-        this.removeClock(
-          "menu",
-          this.button.menu.shadowRoot.querySelector("paper-icon-button"),
-          this.button.menu
-        );
-
         // Hide or show buttons.
-        this.elementStyle(this.cardConfig.menu, this.button.menu, true);
+        this.elementStyle(this.cchConfig.menu, this.button.menu, true);
         this.elementStyle(
-          this.cardConfig.notification,
-          this.button.notification,
+          this.cchConfig.notifications,
+          this.button.notifications,
           true
         );
-        this.elementStyle(this.cardConfig.voice, this.button.voice, true);
-        this.elementStyle(this.cardConfig.options, this.button.options, true);
+        this.elementStyle(this.cchConfig.voice, this.button.voice, true);
+        this.elementStyle(this.cchConfig.options, this.button.options, true);
 
         // Pad bottom for image backgrounds as we're shifted -64px.
         root
           .querySelector("ha-app-layout")
-          .querySelector('[id="view"]').style.paddingBottom = this.cardConfig
+          .querySelector('[id="view"]').style.paddingBottom = this.cchConfig
           .background_image
           ? "64px"
           : "";
-
-        // Set clock width.
-        let clock_w =
-          this.cardConfig.clock_format == 12 && this.cardConfig.clock_am_pm
-            ? 110
-            : 80;
 
         if (this.tab_container) {
           // Add width of all visible elements on right side for tabs margin.
           let pad = 0;
           pad +=
-            this.cardConfig.notification &&
-            this.cardConfig.clock != "notification"
+            this.cchConfig.notifications &&
+            this.cchConfig.clock != "notifications"
               ? 45
               : 0;
           pad +=
-            this.cardConfig.voice && this.cardConfig.clock != "voice" ? 45 : 0;
+            this.cchConfig.voice && this.cchConfig.clock != "voice" ? 45 : 0;
           pad +=
-            this.cardConfig.options && this.cardConfig.clock != "options"
+            this.cchConfig.options && this.cchConfig.clock != "options"
               ? 45
               : 0;
-          if (this.cardConfig.clock && this.cardConfig.clock != "menu") {
+          if (this.cchConfig.clock && this.cchConfig.clock != "menu") {
             pad +=
-              this.cardConfig.clock_am_pm && this.cardConfig.clock_format == 12
+              this.cchConfig.clock_am_pm && this.cchConfig.clock_format == 12
                 ? 110
                 : 80;
           }
           this.tab_container.style.cssText = `margin-right:${pad}px;`;
 
-          // Add margin to left side if menu button is the clock.
-          if (this.cardConfig.menu && this.cardConfig.clock != "menu") {
-            this.tab_container.shadowRoot.getElementById(
-              "tabsContainer"
-            ).style.cssText = "margin-left:60px;";
-          } else if (this.cardConfig.menu && this.cardConfig.clock == "menu") {
-            this.tab_container.shadowRoot.getElementById(
-              "tabsContainer"
-            ).style.cssText = `margin-left:${clock_w + 15}px;`;
-          }
-
           // Shift the header up to hide unused portion.
           root.querySelector("app-toolbar").style.cssText = "margin-top:-64px;";
 
           // Hide tab bar scroll arrows to save space since we can still swipe.
-          this.tab_container.shadowRoot.querySelectorAll(
+          let chevron = this.tab_container.shadowRoot.querySelectorAll(
             '[icon^="paper-tabs:chevron"]'
-          )[0].style.cssText = "display:none;";
-          this.tab_container.shadowRoot.querySelectorAll(
-            '[icon^="paper-tabs:chevron"]'
-          )[1].style.cssText = "display:none;";
+          )
+          chevron[0].style.cssText = "display:none;";
+          chevron[1].style.cssText = "display:none;";
 
           // Hide or show tabs.
           if (config_views && !window.cch_tabs_display) {
             for (let i = 0; i < this.tabs.length; i++) {
               if (config_views.indexOf(String(i)) > -1) {
-                this.elementStyle(this.cardConfig.tabs, this.tabs[i], false);
+                this.elementStyle(this.cchConfig.tabs, this.tabs[i], false);
               } else {
                 this.tabs[i].style.cssText = "display:none;";
               }
@@ -293,94 +249,88 @@ ${navigator.userAgent}</textarea
             }
           } else {
             for (let i = 0; i < this.tabs.length; i++) {
-              this.elementStyle(this.cardConfig.tabs, this.tabs[i], false);
+              this.elementStyle(this.cchConfig.tabs, this.tabs[i], false);
             }
           }
         }
-
-        // Strings to compare config to. Avoids errors while typing in edit field.
-        let clock_strings = ["notification", "voice", "options", "menu"];
-
-        // Get elements to style for clock choice.
-        if (clock_strings.indexOf(this.cardConfig.clock) > -1) {
-          if (this.cardConfig.clock == "notification") {
-            this.clock_icon = this.button.notification.shadowRoot.querySelector(
-              "paper-icon-button"
-            );
-            this.clock_iron_icon = this.button.notification.shadowRoot
-              .querySelector("paper-icon-button")
-              .shadowRoot.querySelector("iron-icon");
-            this.button.notification.shadowRoot.querySelector(
-              '[class="indicator"]'
-            ).style.cssText = "top:14.5px;left:-7px";
-          } else if (this.cardConfig.clock == "voice") {
-            this.clock_icon = this.button.voice.shadowRoot.querySelector(
-              "paper-icon-button"
-            );
-            this.clock_iron_icon = this.button.voice.shadowRoot
-              .querySelector("paper-icon-button")
-              .shadowRoot.querySelector("iron-icon");
-          } else if (this.cardConfig.clock == "options") {
-            this.clock_icon = this.button.options.querySelector(
-              "paper-icon-button"
-            );
-            this.clock_iron_icon = this.button.options
-              .querySelector("paper-icon-button")
-              .shadowRoot.querySelector("iron-icon");
-          } else if (this.cardConfig.clock == "menu") {
-            this.clock_icon = this.button.menu.shadowRoot.querySelector(
-              "paper-icon-button"
-            );
-            this.clock_iron_icon = this.button.menu.shadowRoot
-              .querySelector("paper-icon-button")
-              .shadowRoot.querySelector("iron-icon");
-          }
-
-          // If the clock element doesn't exist yet, create & insert.
-          if (this.cardConfig.clock && clock == null) {
-            let create_clock = document.createElement("p");
-            create_clock.setAttribute("id", "cch_clock");
-            create_clock.style.cssText = `
-            width:${clock_w}px;
-            margin-top:2px;
-            margin-left:-8px;
-          `;
-            this.clock_iron_icon.parentNode.insertBefore(
-              create_clock,
-              this.clock_iron_icon
-            );
-          }
-
-          // Style clock and insert time text.
-          var clock = this.clock_icon.shadowRoot.getElementById("cch_clock");
-          if (this.cardConfig.clock && clock != null) {
-            this.updateClock();
-            this.clock_icon.style.cssText = `
-            margin-right:-5px;
-            width:${clock_w}px;
-            text-align: center;
-          `;
-            this.clock_iron_icon.style.cssText = "display:none;";
-          }
-        }
       }
+      if (this.cchConfig.clock) this.insertClock()
       window.dispatchEvent(new Event("resize"));
     }
   }
 
+  insertClock() {
+    let clock_width =
+      this.cchConfig.clock_format == 12 && this.cchConfig.clock_am_pm
+        ? 110
+        : 80;
+
+    if (!this.raw_config_mode && !this.edit_mode && this.clock == null) {
+      let clock_icon;
+      let clock_iron_icon;
+
+      if (this.cchConfig.clock == "options") {
+        clock_icon = this.button[this.cchConfig.clock].querySelector(
+            "paper-icon-button"
+          );
+      } else {
+        clock_icon = this.button[this.cchConfig.clock].shadowRoot.querySelector(
+            "paper-icon-button"
+          );
+      }
+      clock_iron_icon = clock_icon.shadowRoot.querySelector("iron-icon");
+
+      this.button.notifications.shadowRoot.querySelector(
+          '[class="indicator"]'
+        ).style.cssText = this.cchConfig.clock == "notifications"
+          ? "top:14.5px;left:-7px"
+          : '';
+
+      let clock_element = document.createElement("p");
+      clock_element.setAttribute("id", "cch_clock");
+      clock_element.style.cssText = `
+        width:${clock_width}px;
+        margin-top:2px;
+        margin-left:-8px;
+      `;
+      clock_icon.style.cssText = `
+        margin-right:-5px;
+        width:${clock_width}px;
+        text-align: center;
+      `;
+      clock_iron_icon.parentNode.insertBefore( clock_element, clock_iron_icon );
+      clock_iron_icon.style.cssText = "display:none;";
+
+      this.clock = clock_icon.shadowRoot.getElementById("cch_clock");
+      this.updateClock();
+    }
+
+    if (this.tab_container) {
+      // Add margin to left side of tabs if menu is the clock.
+      if (this.cchConfig.menu && this.cchConfig.clock != "menu") {
+        this.tab_container.shadowRoot.getElementById(
+          "tabsContainer"
+        ).style.cssText = "margin-left:60px;";
+      } else if (this.cchConfig.menu && this.cchConfig.clock == "menu") {
+        this.tab_container.shadowRoot.getElementById(
+          "tabsContainer"
+        ).style.cssText = `margin-left:${clock_width + 15}px;`;
+      }
+    }
+  }
+
   updateClock() {
-    var clock = this.clock_icon.shadowRoot.getElementById("cch_clock");
     let clock_format = {
-      hour12: this.cardConfig.clock_format != 24,
+      hour12: this.cchConfig.clock_format != 24,
       hour: "2-digit",
       minute: "2-digit"
     };
     let date = new Date();
     date = date.toLocaleTimeString([], clock_format);
-    if (!this.cardConfig.clock_am_pm && this.cardConfig.clock_format == 12) {
-      clock.innerHTML = date.slice(0, -3);
+    if (!this.cchConfig.clock_am_pm && this.cchConfig.clock_format == 12) {
+      this.clock.innerHTML = date.slice(0, -3);
     } else {
-      clock.innerHTML = date;
+      this.clock.innerHTML = date;
     }
     var t = window.setTimeout(() => this.updateClock(), 60000);
   }
@@ -419,28 +369,6 @@ ${navigator.userAgent}</textarea
       element.style.cssText = config ? "" : "display:none";
     } else {
       element.style.cssText = "";
-    }
-  }
-
-  // Revert button if previously a clock.
-  removeClock(config, element, parent) {
-    if (
-      config.clock != config &&
-      element.shadowRoot.getElementById("cch_clock") != null
-    ) {
-      let clock_element = element.shadowRoot.getElementById("cch_clock");
-      clock_element.parentNode.querySelector("iron-icon").style.cssText = "";
-      if (config == "options") {
-        parent.querySelector("paper-icon-button").style.cssText = "";
-      } else {
-        parent.shadowRoot.querySelector("paper-icon-button").style.cssText = "";
-      }
-      if (config == "notification") {
-        this.button.notification.shadowRoot.querySelector(
-          '[class="indicator"]'
-        ).style.cssText = "";
-      }
-      clock_element.parentNode.removeChild(clock_element);
     }
   }
 
