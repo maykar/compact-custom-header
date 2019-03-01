85337c099c9a94faaaf6573b7b31cff14f219b40
MKR <contact@ninjacyb.org>
2019-03-01 09:40:32 -0500

    bump

--- compact-custom-header.js
+++ compact-custom-header.js
@@ -1,4 +1,4 @@
-import "./compact-custom-header-editor.js?v=1.0.1b2";
+import "./compact-custom-header-editor.js?v=1.0.1b3";
 
 export const LitElement = Object.getPrototypeOf(
   customElements.get("ha-panel-lovelace")
